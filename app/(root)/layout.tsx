8844ae1e81956cb6dc9ffd1ac1a4a1f267e731ca
Saptarshi <saptarshimondal768@gmail.com>
2024-01-07 23:27:40 +0530

    Head tag added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Head from 'next/head';
 
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
